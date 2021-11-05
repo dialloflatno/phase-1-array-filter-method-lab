ad17e0179ab61857d9cd476ebf8f967e2f4e5bbe
dialloflatno <brahimdiallo23@gmail.com>
2021-11-05 17:00:55 -0400

    commit tree d5cfdbb536fc60cc2d190c91ba6c9f2f7765ce80

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+// * `findMatching`- This function takes an array of drivers' names and a `string`
+//   as arguments, and returns the matching list of drivers. The function should be
+//   case insensitive.
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
+// expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
+
+function findMatching(names, match) {
+    const driverNames =
+        names.filter(eachName =>
+            eachName === match);
+    return driverNames.toLowerCase
+
+}
+
+
+
+
+
+// ///Ingredients///
+// function cakeRecipe(onePoundOfEgg, yeast, frosting) {
+//     //Instruction///
+//     crack == onePoundOfEgg
+//     pourTogheter == yeast
+//     mix == frosting
+//     const bakedCake = final;
+
+//     return bakedCake;
+// }
+
+// cakeRecipe(eggies, originYeast, pillsburyFrosting);
